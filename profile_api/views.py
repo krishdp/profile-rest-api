cd122bee8721f2e7f754e60add31ba99117053ec
Krishnandu Ranjan Roy <roykrish9899@gmail.com>
2022-03-24 15:55:36 +0530

    Added serching to user profile.

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
+
 from profile_api import serializers, models, permissions
 
 
@@ -104,3 +104,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
